cc5556caa1ebdb3d381e7616a4ea75ac499ee736
Lucia <luciafutestti@gmail.com>
2022-05-13 16:39:56 -0300

    Se agrega boton a gráfico

--- src/PricesItem.jsx
+++ src/PricesItem.jsx
@@ -3,25 +3,31 @@ import React, {useEffect, useState} from "react";
 const PricesItem = ({ crypto}) => {
 
     const [color, setColor] = useState("");
+    const url = `https://www.binance.com/es/trade/${crypto.symbol}_BUSD?theme=dark&type=spot`;
 
-    const conditionalColor = () => {
+    useEffect(() => {
+      const conditionalColor = () => {
         if (crypto.changePercent24Hr > 0) {
             setColor("green");
         } else {
             setColor("red");
         }
-        console.log(color); 
       };
-
-    useEffect(() => {
-        conditionalColor();
+      conditionalColor();
     }, [crypto]);
 
   return (
       <div className="card  animate__animated animate__zoomIn" style={{backgroundColor: color}}>
         <h2>{crypto.symbol}</h2>
         <h6>{crypto.name}</h6>
-        <h3>${parseFloat(crypto.priceUsd).toFixed(2)}USD / {parseFloat(crypto.changePercent24Hr).toFixed(2)}% </h3>
+        <h3>${parseFloat(crypto.priceUsd).toFixed(2)} USD / {parseFloat(crypto.changePercent24Hr).toFixed(2)}% </h3>
+        <div className="d-grid gap-2 col-6 mx-auto pt-2">
+          <a href={url} target="_blank">
+            <button className="btn" type="button">
+              Ver Gráfico
+            </button>
+          </a>
+        </div>
       </div>
   );
 };
