e2a916bcaea22d21071ff213656ffd942b75f3c5
Lucia <luciafutestti@gmail.com>
2022-05-16 16:41:42 -0300

    se configura responsive

--- src/Carousel.jsx
+++ src/Carousel.jsx
@@ -18,8 +18,39 @@ const Carousel = () => {
           infinite: true,
           slidesToShow: 4,
           slidesToScroll: 4,
+          autoplay: true,
+          speed: 3000,
+          autoplaySpeed: 3000,
+          cssEase: "linear",
+          pauseOnHover: true,
           adaptiveHeight : true,
           accesibility : true,
+            responsive: [
+                {
+                    breakpoint: 1024,
+                    settings: {
+                        slidesToShow: 3,
+                        slidesToScroll: 3,
+                        infinite: true,
+                        dots: true
+                    }
+                },
+                {
+                    breakpoint: 600,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 2,
+                        initialSlide: 2
+                    }
+                },
+                {
+                    breakpoint: 480,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1
+                    }
+                }
+            ]
         };
         return (
             <>
